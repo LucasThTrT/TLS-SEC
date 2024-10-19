99847f94d3f47869b12d488714f706e0ce209e07
Lucas THIETART <thietartl@gmail.com>
2024-10-20 00:48:34 +0200

    lecture de tous les outils

--- Projet_C/main.c
+++ Projet_C/main.c
@@ -1,10 +1,48 @@
 #include "structs.h"
 
 
+void init_outils(section* s){
+    // printf("Initialisation de la structure outils\n");
+    // Lecture des fichiers dans les sections
+    // dossier parent = sections/section->name
+    char folder[50] = "sections/";
+    strcat(folder, s->name);
+    // printf("Lecture des fichiers dans le dossier %s\n", folder);
+    char** liste = read_folder(folder);
+
+    // Initialisation de la liste chaîné outils
+    s->first = malloc(sizeof(outils));
+    s->actuel = s->first;
+
+    int i = 0;
+    while (liste[i] != NULL){
+        if(i == 0){
+            // printf("Initialisation de l'outil %s\n", liste[i]);
+            // Initialisation
+            // s->first->id = malloc(sizeof(int));                       //? utile ///
+            s->first->name = malloc(strlen(liste[i]) * sizeof(char)); //? utile ///
+            s->first->id = i;
+            strcpy(s->first->name, liste[i]);
+            s->first->next = NULL;
+        }
+        else{
+            s->actuel->next = malloc(sizeof(outils));
+            s->actuel = s->actuel->next;
+            // s->actuel->id = malloc(sizeof(int));                       //? utile ///
+            s->actuel->name = malloc(strlen(liste[i]) * sizeof(char)); //? utile ///
+            s->actuel->id = i;
+            strcpy(s->actuel->name, liste[i]);
+            s->actuel->next = NULL;
+        }
+        i++;
+    }
+}
+
 void init_frog(froger *frog){
-    printf("Initialisation de la structure froger\n");
+
+    // printf("Initialisation de la structure froger\n");
     // Initialisation de la liste chaînée section
-    // Lecture des fichiers dans le dossier section
+    // Lecture des fichiers dans le dossier sections
     char* folder = "sections";
     char** liste = read_folder(folder);
     section* s = malloc(sizeof(section));
@@ -18,12 +56,19 @@ void init_frog(froger *frog){
         // affichage de la liste des sections
         // printf("Section %d -> %s\n", i, liste[i]);
         if (i == 0){
+            // SECTION
             // allocation de la mémoire pour le nom de la section
             copie->name = malloc(strlen(liste[i]) * sizeof(char));
             // copie de la chaine de caractère
             strcpy(copie->name, liste[i]);
+
+            // OUTILS
+            // Initialisation de la liste chaîné outils
+            init_outils(copie);
+            copie->next = NULL;
         }
         else{
+            // SECTION
             copie->next = malloc(sizeof(section));
             copie = copie->next;
             // allocation de la mémoire pour le nom de la section
@@ -31,11 +76,13 @@ void init_frog(froger *frog){
             // copie de la chaine de caractère
             strcpy(copie->name, liste[i]); //! pas faire le strcpy risque de segfault
             copie->next = NULL;
+
+            // OUTILS
+            // Initialisation de la liste chaîné outils
+            init_outils(copie);
         }
         i++;
     }
-
-    //TODO : Maintenant il faut dans chaque itération while faire la même opération pour les outils !!
 }
 
 
@@ -50,17 +97,17 @@ int main(void){
     init_frog(frog);
 
     // Test de l'initialisation de la structure froger
-    // affichage de toutes les sections
+    // affichage de toutes les sections et outils
     section* s = frog->first;
-    int i = 0;
-    while (s != NULL)
-    {
-        printf("Section %d -> %s\n", i, s->name);
+    while (s != NULL){
+        printf("Section : %s\n", s->name);
+        outils* o = s->first;
+        while (o != NULL){
+            printf("Outil : %s\n", o->name);
+            o = o->next;
+        }
         s = s->next;
-        i++;
     }
-
-    return 0;
 }
 
 
